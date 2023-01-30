d7f386816de5c1ca64f6a19f26f0404cd0d581b0
deepika11 <chaturvedideepika69@gmail.com>
2023-01-30 19:44:22 +0530

    first commit

---
+++ client.js
@@ -0,0 +1,19 @@
+const baseUrl = "https://jsonplaceholder.typicode.com"
+
+function handleResponse(apiData){
+  var data = {
+    data:apiData
+  };
+  render(document, data)
+}
+
+function getData(url) {
+  fetch(baseUrl + url +"/")
+    .then((response) => response.json())
+    .then((json) => {
+      return handleResponse(json)
+    })
+    
+  }
+getData(window.location.pathname.split('.')[0])
+// getData("posts")
