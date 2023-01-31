56dc11cb9028f4177f0b896fe27583498e684439
deepika11 <chaturvedideepika69@gmail.com>
2023-01-31 19:24:12 +0530

    31jan 2023

--- client.js
+++ client.js
@@ -1,21 +1,22 @@
 const baseUrl = "https://jsonplaceholder.typicode.com"
-
 function handleResponse(apiData){
   var data = {
     data:apiData
   };
   render(document, data)
 }
-
 function getData(url) {
-  fetch(baseUrl + url +"/")
-
+  fetch(baseUrl + url +"//")
     .then((response) => response.json())
     .then((json) => {
       return handleResponse(json)
     })
-    
   }
-  
+
+function postData(apiData){
+  debugger
+  apiData
+}
+postData()
 getData(window.location.pathname.split('.')[0])
 // getData("posts")
--- renderer.js
+++ renderer.js
@@ -2,10 +2,8 @@ function render(d, data) {
   // Prepare template
   var templateSource = d.getElementById('myTemplate').innerHTML;
   var template = Handlebars.compile(templateSource);
-
   var html = template(data);
 
   // Add the result to the DOM
-  
   d.getElementById('renderedContent').innerHTML = html;
 }
